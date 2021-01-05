b62ac68e8b46afb0c6b2c2262582cc1d1aa94210
Jorge Gabitto <jgabitto1792@gmail.com>
2021-01-05 11:03:13 -0600

    Added swagger

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require "swagger_helper"
 
 RSpec.describe 'Items API', type: :request do
   # Initialize the test data
@@ -123,5 +124,72 @@ RSpec.describe 'Items API', type: :request do
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
     end
+  end
+
+  path '/todos/{todo_id}/items' do
+
+    parameter name: 'todo_id', in: :path, type: :integer, description: 'todo_id'
+
+    get('list items') do
+      tags 'Items'
+      response(200, 'success') do
+        run_test!
+      end
+    end
+
+    post('create item') do
+      tags 'Items'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :item, in: :body, required: true, schema: {
+        type: :object,
+        required: %i[name],
+        properties: {
+          name: {type: :string},
+          done: {type: :boolean}
+        }
+      }
+      response(201, 'success') do
+        let(:item) { { name: 'thisitem', done: false } }
+        run_test!
+      end
+    end
+  end
+
+  path '/todos/{todo_id}/items/{id}' do
+    parameter name: 'todo_id', in: :path, type: :integer, description: 'todo_id'
+    parameter name: 'id', in: :path, type: :integer, description: 'id'
+
+    get('show item') do
+      tags 'Items'
+      response(200,'success') do
+        run_test!
+      end
+    end
+
+    put('update item') do
+      tags 'Items'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :item, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          name: {type: :string},
+          done: {type: :boolean}
+        }
+      }
+      response(204,'success') do
+        let(:item) { { name: 'changedName'} }
+        run_test!
+      end
+    end
+
+    delete('delete item') do
+      tags 'Items'
+      response(204,'success') do
+        run_test!
+      end
+    end
+
   end
 end
